b703c579b05de91e05818c984dbf543309b40e77
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-09-23 20:09:56 +0200

    feat: add tests for all situation with url and internet

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+from unittest.mock import patch
+from app.main import can_access_google_page
+
+
+@patch("app.main.has_internet_connection", return_value=True)
+@patch("app.main.valid_google_url", return_value=True)
+def test_accessible(mock_url: str, mock_internet: None) -> None:
+    assert can_access_google_page("https://google.com") == "Accessible"
+
+
+@patch("app.main.has_internet_connection", return_value=False)
+@patch("app.main.valid_google_url", return_value=True)
+def test_no_internet(mock_url: str, mock_internet: None) -> None:
+    assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+@patch("app.main.has_internet_connection", return_value=True)
+@patch("app.main.valid_google_url", return_value=False)
+def test_invalid_url(mock_url: str, mock_internet: None) -> None:
+    assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+@patch("app.main.has_internet_connection", return_value=False)
+@patch("app.main.valid_google_url", return_value=False)
+def test_no_internet_and_invalid_url(
+        mock_url: str,
+        mock_internet: None
+) -> None:
+    assert can_access_google_page("https://google.com") == "Not accessible"
