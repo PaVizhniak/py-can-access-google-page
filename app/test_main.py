b3e954936ea5c7bdf13fb41bdf1abf4da780af42
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-09-24 18:11:40 +0200

    refactor: change type hit for atributes tests and name of tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,38 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from app.main import can_access_google_page
 
 
 @patch("app.main.has_internet_connection", return_value=True)
 @patch("app.main.valid_google_url", return_value=True)
-def test_accessible(mock_url: str, mock_internet: None) -> None:
+def test_valid_url_and_connection_exists_returns_accessible(
+        mock_url: MagicMock,
+        mock_internet: MagicMock
+) -> None:
     assert can_access_google_page("https://google.com") == "Accessible"
 
 
 @patch("app.main.has_internet_connection", return_value=False)
 @patch("app.main.valid_google_url", return_value=True)
-def test_no_internet(mock_url: str, mock_internet: None) -> None:
+def test_valid_url_and_no_connection_exists_returns_not_accessible(
+        mock_url: MagicMock,
+        mock_internet: MagicMock
+) -> None:
     assert can_access_google_page("https://google.com") == "Not accessible"
 
 
 @patch("app.main.has_internet_connection", return_value=True)
 @patch("app.main.valid_google_url", return_value=False)
-def test_invalid_url(mock_url: str, mock_internet: None) -> None:
+def test_invalid_url_and_connection_exists_returns_not_accessible(
+        mock_url: MagicMock,
+        mock_internet: MagicMock
+) -> None:
     assert can_access_google_page("https://google.com") == "Not accessible"
 
 
 @patch("app.main.has_internet_connection", return_value=False)
 @patch("app.main.valid_google_url", return_value=False)
-def test_no_internet_and_invalid_url(
-        mock_url: str,
-        mock_internet: None
+def test_invalid_url_and_no_connection_exists_returns_not_accessible(
+        mock_url: MagicMock,
+        mock_internet: MagicMock
 ) -> None:
     assert can_access_google_page("https://google.com") == "Not accessible"
